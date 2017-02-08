144b3066fa206474893bfe45e1f75dfb396911ab
Lee HanYeong <dev@azelf.com>
2017-02-08 16:35:21 +0900

    index view작성, polls:detail url태그 사용하도록 변경

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 
@@ -7,16 +8,25 @@ def index(request):
     # pub_date컬럼(필드)를 기준으로 내림차순 정렬한 결과를 latest_question_list에 할당
     latest_question_list = Question.objects.order_by('-pub_date')
 
-    # 템플릿 로더를 이용해서 'polls/index.html'에서 템플릿파일을 가져온다
-    template = loader.get_template('polls/index.html')
-
-    # 템플릿 파일에 전달할 context객체를 정의
     context = {
-        # 'latest_question_list'라는 키에 값을 할당, 해당 키로 템플릿에서 사용가능하다
         'latest_question_list': latest_question_list,
     }
-    # 템플릿에 context와 request객체를 사용해서 render해준 결과를 돌려줌
-    return HttpResponse(template.render(context, request))
+    ## shortcut을 이용해 짧게 처리
+    # 'polls/index.html' 템플릿을 이용해서
+    # request, context객체를 이용해 HttpResponse를 반환
+    return render(request, 'polls/index.html', context)
+
+    ## loader.get_template, template.render를 사용 (과정 2번 거침)
+    # # 템플릿 로더를 이용해서 'polls/index.html'에서 템플릿파일을 가져온다
+    # template = loader.get_template('polls/index.html')
+    #
+    # # 템플릿 파일에 전달할 context객체를 정의
+    # context = {
+    #     # 'latest_question_list'라는 키에 값을 할당, 해당 키로 템플릿에서 사용가능하다
+    #     'latest_question_list': latest_question_list,
+    # }
+    # # 템플릿에 context와 request객체를 사용해서 render해준 결과를 돌려줌
+    # return HttpResponse(template.render(context, request))
 
 
     # 돌려줄 문자열을 작성
